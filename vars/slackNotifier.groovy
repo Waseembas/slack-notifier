1d37fe2cc56a211bbd4bd3a5e7bb7f9eec5a9c71
Waseembas <47210393+Waseembas@users.noreply.github.com>
2022-03-05 01:52:01 -0800

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -5,7 +5,9 @@ def call(String buildResult) {
     slackSend color: "good", message: "COMMITER: ${env.GIT_AUTHOR} with hash ${env.GIT_LAST_COMMIT} with buildnumber ${env.BUILD_NUMBER} on build Job ${env.JOB_NAME} was successful (<${env.BUILD_URL}|Open>)"
   }
   else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "COMMITER: ${env.GIT_AUTHOR} with hash ${env.GIT_LAST_COMMIT} with buildnumber ${env.BUILD_NUMBER} on build Job ${env.JOB_NAME} was failed (<${env.BUILD_URL}|Open>)"
+
+   slackSend color: "danger", message: "${env.JOB_NAME} » ${env.branch} ${env.GIT_LAST_COMMIT} configure context test cases » build (<${env.BUILD_URL}|${env.BUILD_NUMBER}>) » Started by user ${env.GIT_AUTHOR}"
+  
   }
   else if( buildResult == "UNSTABLE" ) { 
     slackSend color: "warning", message: "COMMITER: ${env.GIT_AUTHOR} with hash ${env.GIT_LAST_COMMIT} with buildnumber ${env.BUILD_NUMBER} on build Job ${env.JOB_NAME} was unstable (<${env.BUILD_URL}|Open>)"
