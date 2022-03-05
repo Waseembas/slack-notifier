d96e7610942c688290f65872f63161e3665f7347
Waseembas <47210393+Waseembas@users.noreply.github.com>
2022-03-05 01:48:27 -0800

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,19 +1,16 @@
-
 #!/usr/bin/env groovy
 
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "${env.JOB_NAME} » ${env.branch} ${env.GIT_LAST_COMMIT} configure context test cases » build (<${env.BUILD_URL}|${env.BUILD_NUMBER}>) » Started by user ${env.GIT_AUTHOR}"
+    slackSend color: "good", message: "COMMITER: ${env.GIT_AUTHOR} with hash ${env.GIT_LAST_COMMIT} with buildnumber ${env.BUILD_NUMBER} on build Job ${env.JOB_NAME} was successful (<${env.BUILD_URL}|Open>)"
   }
   else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "${env.JOB_NAME} » ${env.branch} ${env.GIT_LAST_COMMIT} configure context test cases » build (<${env.BUILD_URL}|${env.BUILD_NUMBER}>) » Started by user ${env.GIT_AUTHOR}"
+    slackSend color: "danger", message: "COMMITER: ${env.GIT_AUTHOR} with hash ${env.GIT_LAST_COMMIT} with buildnumber ${env.BUILD_NUMBER} on build Job ${env.JOB_NAME} was failed (<${env.BUILD_URL}|Open>)"
   }
   else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "${env.JOB_NAME} » ${env.branch} ${env.GIT_LAST_COMMIT} configure context test cases » build (<${env.BUILD_URL}|${env.BUILD_NUMBER}>) » Started by user ${env.GIT_AUTHOR}"
+    slackSend color: "warning", message: "COMMITER: ${env.GIT_AUTHOR} with hash ${env.GIT_LAST_COMMIT} with buildnumber ${env.BUILD_NUMBER} on build Job ${env.JOB_NAME} was unstable (<${env.BUILD_URL}|Open>)"
   }
   else {
-    slackSend color: "danger", message: "${env.JOB_NAME} » ${env.branch} ${env.GIT_LAST_COMMIT} configure context test cases » build (<${env.BUILD_URL}|${env.BUILD_NUMBER}>) » Started by user ${env.GIT_AUTHOR}"	
+    slackSend color: "danger", message: "COMMITER: ${env.GIT_AUTHOR} with hash ${env.GIT_LAST_COMMIT} with buildnumber ${env.BUILD_NUMBER} on build Job ${env.JOB_NAME} its result was canceled (<${env.BUILD_URL}|Open>)"	
   }
 }
-
-
