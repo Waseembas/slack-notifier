4d236bbf4e97564fed8a2ad92f1d6806078db93a
Waseembas <47210393+Waseembas@users.noreply.github.com>
2022-03-05 02:14:51 -0800

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -5,8 +5,8 @@ def call(String buildResult) {
     slackSend color: "good", message: "COMMITER: ${env.GIT_AUTHOR} with hash ${env.GIT_LAST_COMMIT} with buildnumber ${env.BUILD_NUMBER} on build Job ${env.JOB_NAME} was successful (<${env.BUILD_URL}|Open>)"
   }
   else if( buildResult == "FAILURE" ) { 
-
-   slackSend color: "danger", message: "${env.JOB_NAME} » ${env.branch} ${env.GIT_LAST_COMMIT} configure context test cases » build (<${env.BUILD_URL}|${env.BUILD_NUMBER}>) » Started by user ${env.GIT_AUTHOR}"
+https://gitlab.com/qampus3/be/qampus-be/-/commit
+   slackSend color: "danger", message: "${env.JOB_NAME} » ${env.branch}   (https://gitlab.com/qampus3/be/qampus-be/-/commit/${env.GIT_LAST_COMMIT} |${env.GIT_LAST_COMMIT}>)  configure context test cases » build (<${env.BUILD_URL}|${env.BUILD_NUMBER}>) » Started by user ${env.GIT_AUTHOR}"
   
   }
   else if( buildResult == "UNSTABLE" ) { 
