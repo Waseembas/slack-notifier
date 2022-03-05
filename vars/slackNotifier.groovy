a6b344817819994dcf564e69e339117bc3bece34
Waseembas <47210393+Waseembas@users.noreply.github.com>
2022-03-05 02:25:46 -0800

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -2,17 +2,16 @@
 
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "COMMITER: ${env.GIT_AUTHOR} with hash ${env.GIT_LAST_COMMIT} with buildnumber ${env.BUILD_NUMBER} on build Job ${env.JOB_NAME} was successful (<${env.BUILD_URL}|Open>)"
+    slackSend color: "danger", message: "${env.JOB_NAME} » ${env.branch} (${env.GIT_BASE}/${env.GIT_LAST_COMMIT} |${env.GIT_LAST_COMMIT}>)  configure context test cases » build (<${env.BUILD_URL}|${env.BUILD_NUMBER}>) » Started by user ${env.GIT_AUTHOR}"
   }
   else if( buildResult == "FAILURE" ) { 
-https://gitlab.com/qampus3/be/qampus-be/-/commit
-   slackSend color: "danger", message: "${env.JOB_NAME} » ${env.branch}   (https://gitlab.com/qampus3/be/qampus-be/-/commit/${env.GIT_LAST_COMMIT} |${env.GIT_LAST_COMMIT}>)  configure context test cases » build (<${env.BUILD_URL}|${env.BUILD_NUMBER}>) » Started by user ${env.GIT_AUTHOR}"
+    slackSend color: "danger", message: "${env.JOB_NAME} » ${env.branch} (${env.GIT_BASE}/${env.GIT_LAST_COMMIT} |${env.GIT_LAST_COMMIT}>)  configure context test cases » build (<${env.BUILD_URL}|${env.BUILD_NUMBER}>) » Started by user ${env.GIT_AUTHOR}"
   
   }
   else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "COMMITER: ${env.GIT_AUTHOR} with hash ${env.GIT_LAST_COMMIT} with buildnumber ${env.BUILD_NUMBER} on build Job ${env.JOB_NAME} was unstable (<${env.BUILD_URL}|Open>)"
+    slackSend color: "danger", message: "${env.JOB_NAME} » ${env.branch} (${env.GIT_BASE}/${env.GIT_LAST_COMMIT} |${env.GIT_LAST_COMMIT}>)  configure context test cases » build (<${env.BUILD_URL}|${env.BUILD_NUMBER}>) » Started by user ${env.GIT_AUTHOR}"
   }
   else {
-    slackSend color: "danger", message: "COMMITER: ${env.GIT_AUTHOR} with hash ${env.GIT_LAST_COMMIT} with buildnumber ${env.BUILD_NUMBER} on build Job ${env.JOB_NAME} its result was canceled (<${env.BUILD_URL}|Open>)"	
+    slackSend color: "danger", message: "${env.JOB_NAME} » ${env.branch} (${env.GIT_BASE}/${env.GIT_LAST_COMMIT} |${env.GIT_LAST_COMMIT}>)  configure context test cases » build (<${env.BUILD_URL}|${env.BUILD_NUMBER}>) » Started by user ${env.GIT_AUTHOR}"
   }
 }
